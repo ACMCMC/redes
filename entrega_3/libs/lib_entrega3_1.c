77a98f46d33518507930876e745246ece1fe38e5
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2020-11-13 13:45:26 +0100

    Comentada la parte del receptor en la P3

--- entrega_3/libs/lib_entrega3_1.c
+++ entrega_3/libs/lib_entrega3_1.c
@@ -81,8 +81,8 @@ Esta función se conecta al servidor con el puerto_propio y la dirección que se
 int receptor(char *puerto)
 {
 
-    int id_socket_propio;                            // El número de socket para la conexión con el servidor
-    struct sockaddr_in socket_sender, socket_propio; // Esto lo usaremos para conectarnos con el otro host (el servidor). La dirección la sacaremos de los argumentos de la función.
+    int id_socket_propio;                            // El número de socket que escuchará conexiones
+    struct sockaddr_in socket_sender, socket_propio; // socket_sender guardará la dirección del que envía el mensaje; socket_propio guarda la del socket de este host
     socklen_t tam_socket_sender;
     char mensaje_recibido[MAX_TAM_MSG]; // Aquí guardaremos el mensaje que recibimos
     ssize_t bytes_recibidos;            // Lo usaremos para saber la longitud del mensaje recibido
@@ -90,7 +90,7 @@ int receptor(char *puerto)
 
     printf("Este host es el que recibe. Vamos a escuchar en el puerto " ANSI_COLOR_CYAN "%s" ANSI_COLOR_RESET ".\n", puerto);
 
-    id_socket_propio = socket(AF_INET, SOCK_DGRAM, 0); // Creamos un socket para conectarnos al servidor
+    id_socket_propio = socket(AF_INET, SOCK_DGRAM, 0); // Creamos un socket para escuchar por conexiones
 
     if (id_socket_propio < 0)
     {
@@ -98,40 +98,40 @@ int receptor(char *puerto)
         return (EXIT_FAILURE);
     }
 
-    memset(&socket_propio, 0, sizeof(struct sockaddr_in));
-    socket_propio.sin_family = AF_INET;
-    socket_propio.sin_port = htons(atoi(puerto));
-    socket_propio.sin_addr.s_addr = INADDR_ANY;
+    memset(&socket_propio, 0, sizeof(struct sockaddr_in)); // Llenamos de 0s la estructura del socket propio
+    socket_propio.sin_family = AF_INET; // IPv4
+    socket_propio.sin_port = htons(atoi(puerto)); // El puerto de escucha
+    socket_propio.sin_addr.s_addr = INADDR_ANY; // Escuchamos por cualquier interfaz
 
-    if (bind(id_socket_propio, (struct sockaddr *)&socket_propio, sizeof(socket_propio)) < 0)
+    if (bind(id_socket_propio, (struct sockaddr *)&socket_propio, sizeof(socket_propio)) < 0) // Asignamos el puerto que elegimos arriba al socket que declaramos antes también
     {
         perror("No se ha podido asignar la dirección al socket");
         return (EXIT_FAILURE);
     }
 
-    memset(&socket_sender, 0, sizeof(struct sockaddr_in));
-    socket_sender.sin_family = AF_INET;
-    tam_socket_sender = sizeof(struct sockaddr_in);
+    memset(&socket_sender, 0, sizeof(struct sockaddr_in)); // Inicializamos la estructura que guardará la dirección del que envía a todo 0s
+    tam_socket_sender = sizeof(struct sockaddr_in); // El tamaño de la estructura que guarda la dirección del que envía. No es realmente necesaria la variable, ya que podríamos poner el valor directamente en recvfrom(), pero creo que es más claro declararlo de forma explícita
 
-    bytes_recibidos = recvfrom(id_socket_propio, mensaje_recibido, sizeof(mensaje_recibido), 0, (struct sockaddr *)&socket_sender, &tam_socket_sender);
+    bytes_recibidos = recvfrom(id_socket_propio, mensaje_recibido, sizeof(mensaje_recibido), 0, (struct sockaddr *)&socket_sender, &tam_socket_sender); // Recibimos un mensaje a través del socket que hemos abierto
+
+    close(id_socket_propio); // Cerramos el socket de conexión al servidor
 
     if (bytes_recibidos < 0) // Miramos si hubo error recibiendo el mensaje
     {
         perror("Error al recibir el mensaje");
-        close(id_socket_propio); // Cerramos el socket de conexión al servidor
         return (EXIT_FAILURE);
     }
 
-    close(id_socket_propio); // Cerramos el socket de conexión al servidor
-
-    ip_sender = (char *)realloc(ip_sender, (socket_sender.sin_family == AF_INET6) ? sizeof(char) * INET6_ADDRSTRLEN : sizeof(char) * INET_ADDRSTRLEN);
-    if (inet_ntop(socket_sender.sin_family, (void *)&(socket_sender.sin_addr), ip_sender, (socket_sender.sin_family == AF_INET6) ? sizeof(char) * INET6_ADDRSTRLEN : sizeof(char) * INET_ADDRSTRLEN) == NULL)
+    ip_sender = (char *)realloc(ip_sender, (socket_sender.sin_family == AF_INET6) ? sizeof(char) * INET6_ADDRSTRLEN : sizeof(char) * INET_ADDRSTRLEN); // Guardamos espacio para la IP en formato string, dependiendo de si es IPv4 o IPv6 (en esta práctica sólo usamos IPv4, pero así es más general)
+    if (inet_ntop(socket_sender.sin_family, (void *)&(socket_sender.sin_addr), ip_sender, (socket_sender.sin_family == AF_INET6) ? sizeof(char) * INET6_ADDRSTRLEN : sizeof(char) * INET_ADDRSTRLEN) == NULL) // Guardamos la IP del que envía en ip_sender, como string
     {
         perror("Error en inet_ntop");
         return (EXIT_FAILURE);
     }
 
-    printf(ANSI_COLOR_GREEN "Recibidos %zd bytes de " ANSI_COLOR_BLUE "%s" ANSI_COLOR_RESET ":" ANSI_COLOR_CYAN "%d" ANSI_COLOR_RESET ": %s\n", bytes_recibidos, ip_sender, ntohs(socket_sender.sin_port), mensaje_recibido);
+    printf(ANSI_COLOR_GREEN "Recibidos %zd bytes de " ANSI_COLOR_BLUE "%s" ANSI_COLOR_RESET ":" ANSI_COLOR_CYAN "%d" ANSI_COLOR_RESET ": %s\n", bytes_recibidos, ip_sender, ntohs(socket_sender.sin_port), mensaje_recibido); // Imprimimos IP, puerto y mensaje
+
+    free(ip_sender); // Liberamos el string de la IP del que envía
 
     return (EXIT_SUCCESS);
 }
