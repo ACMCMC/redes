1aa48445d62cfd68dc9f2c5095861c6a54434209
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2020-11-25 15:27:08 +0100

    Comentado el código

--- entrega_3/libs/lib_entrega3_2.h
+++ entrega_3/libs/lib_entrega3_2.h
@@ -5,7 +5,8 @@
 Función que realiza el lado del cliente del apartado 2 de la práctica.
 file: el archivo con los caracteres a enviar
 host: la IP del servidor de mayúsculas
-puerto: el puerto de escucha en el servidor
+puerto_servidor: el puerto de escucha en el servidor
+puerto_propio: el puerto desde donde enviaremos los datagramas UDP
 */
 int cliente_mayusculas(char* file, char* host, char* puerto_propio, char* puerto_servidor);
 
